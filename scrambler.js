691d07b0c144912279878cbf9b87439d2a5c2798
Alec Huang <ahsihuang@gmail.com>
2020-10-30 14:17:33 -0400

    added scrolling animations

--- scrambler.js
+++ scrambler.js
@@ -1,20 +1,14 @@
 
 $(window).on( "load", function() {
-        $(window).on("scroll", function(){
-            if($(window).scrollTop() === "300px"){
-                $(window).off("scroll");
-                $("#arrow1").addClass('animate__animated animate__bounceInLeft').show();
-                $("#arrow2").addClass('rotate animate__animated animate__bounceInRight').show();
-                $("#teddy-bio").addClass('rotate animate__animated animate__bounceInLeft').show();
-                $("#alec-bio").addClass('rotate animate__animated animate__bounceInRight').show();
-            }
-        });
+        ScrollReveal().reveal('.proj-container',{ delay: 500 })
+        //ScrollReveal().reveal('#goodbye-message',{ delay: 500 })
+
         var $randomnbr = $('.nbr');
         var $timer= 10;
         var $data = 0;
         var letters = ["A", "L", "E", "C", "-", "H", "U", "A", "N", "G", "-", "L", "A", "B", "S"];
         $randomnbr.each(function(){
-            var change = Math.round(Math.random()*300);
+            var change = Math.round(Math.random()*250);
             $(this).attr('data-change', change)
         });
         function random(){
@@ -46,29 +40,50 @@ $(window).on( "load", function() {
                     }
                 }
             })
-        
         }
+        /*
+        $(".down-btn").click(function () {
+            $('#aboutme-container').scrollIntoView()
+        })
+        */
+        setInterval(value, $timer);
         $("#arrow1").hide();
         $("#arrow2").hide();
         $("#teddy-bio").hide();
         $("#alec-bio").hide();
-        setInterval(value, $timer);
+        $(document).scroll(function() {
+            if($(this).scrollTop() >= $('#logo-container').offset().top) {
+                $('#arrow1').addClass('animate__animated animate__bounceInLeft').show();
+                $("#arrow2").addClass('rotate animate__animated animate__bounceInRight').show();
+                $("#teddy-bio").addClass('rotate animate__animated animate__bounceInLeft').show();
+                $("#alec-bio").addClass('rotate animate__animated animate__bounceInRight').show();
+            }
+        });
+    
+        /*
+        
+        */
+        
+        /*
         $("#alecTed").click(function() {
-            $("#arrow1").addClass('animate__animated animate__bounceInLeft').show();
-            $("#arrow2").addClass('rotate animate__animated animate__bounceInRight').show();
-            $("#teddy-bio").addClass('rotate animate__animated animate__bounceInLeft').show();
-            $("#alec-bio").addClass('rotate animate__animated animate__bounceInRight').show();
-            return false;
+            //$("#arrow1").addClass('animate__animated animate__bounceInLeft').show();
+            //$("#arrow2").addClass('rotate animate__animated animate__bounceInRight').show();
+            //$("#teddy-bio").addClass('rotate animate__animated animate__bounceInLeft').show();
+            //$("#alec-bio").addClass('rotate animate__animated animate__bounceInRight').show();
         }) 
+        */
         new CircleType(document.getElementById('curved-text')).radius(200);
-        var profPicWidth = $("#alecTed").width();
-        $("#alecTed").css({'height': profPicWidth+'px'});
-        //$('#curved-text').circleType({radius:135});
-        $("#strava-desc").html("<p>The Map on the left displays my cumulative running routes. It is made with leaflet.js along with the mapbox light plug in. The Calender Heatmap on the right is made with D3.js.I Retrieved my personal running data by working with the Strava Api. <br><br> View my Github Repo <a class = \"code-link\" href=\"https://github.com/alec-huang-labs/stravaMapCal\" target=\"_blank\">HERE</a>.</p>")
-        $("#calc-desc").html("<p>Scientific Calculator with an interface inspired by computer terminals from the 1980s. Interface is built with React. Utilizes math.js for it's calculations. <br><br> View my Github Repo <a class = \"code-link\" href=\"https://github.com/alec-huang-labs/TerminalCalculator2.0\" target=\"_blank\">HERE</a>.</p>")
-        $("#md-desc").html("<p>Preview window will dynamically render the output from a given input written in the Markdown language. Utilitzed marked.js as the markdown parser and compiler.<br><br> View my Github Repo <a class = \"code-link\" href=\"https://github.com/alec-huang-labs/markdownPreviewer\" target=\"_blank\">HERE</a>.</p>")
-        $("#quotes-desc").html("<p>First Complete Front-End Project I've ever built!When the Twitter button is pressed, user will be directed to twitter where the tweet will be pre-populated with the text and author of the quote.<br><br> View my Github Repo <a class = \"code-link\" href=\"https://github.com/alec-huang-labs/RandomQuotes\" target=\"_blank\">HERE</a>.</p>")
-        $("#site-desc").html("<p>Animation built with JQuery on the landing page along with lots of SVG graphics to give the site a personal feel. A place to display myself (and Teddy) as well as my work to the world.<br><br> View my Github Repo <a class = \"code-link\" href=\"\" target=\"_blank\">HERE</a>.</p>")
+
+        $("#strava-desc").html(
+            "<p>The Map on the left displays my cumulative running routes. It is made with leaflet.js along with the mapbox light plug in. The Calender Heatmap on the right is made with D3.js.I Retrieved my personal running data by working with the Strava Api. <br><br> View my Github Repo <a class = \"code-link\" href=\"https://github.com/alec-huang-labs/stravaMapCal\" target=\"_blank\">HERE</a>.</p>")
+        $("#calc-desc").html(
+            "<p>Scientific Calculator with an interface inspired by computer terminals from the 1980s. Interface is built with React. Utilizes math.js for it's calculations. <br><br> View my Github Repo <a class = \"code-link\" href=\"https://github.com/alec-huang-labs/TerminalCalculator2.0\" target=\"_blank\">HERE</a>.</p>")
+        $("#md-desc").html(
+            "<p>Preview window will dynamically render the output from a given input written in the Markdown language. Utilitzed marked.js as the markdown parser and compiler.<br><br> View my Github Repo <a class = \"code-link\" href=\"https://github.com/alec-huang-labs/markdownPreviewer\" target=\"_blank\">HERE</a>.</p>")
+        $("#quotes-desc").html(
+            "<p>First Complete Front-End Project I've ever built!When the Twitter button is pressed, user will be directed to twitter where the tweet will be pre-populated with the text and author of the quote.<br><br> View my Github Repo <a class = \"code-link\" href=\"https://github.com/alec-huang-labs/RandomQuotes\" target=\"_blank\">HERE</a>.</p>")
+        $("#site-desc").html(
+            "<p>Animation built with JQuery on the landing page along with lots of SVG graphics to give the site a personal feel. A place to display myself (and Teddy) as well as my work to the world.<br><br> View my Github Repo <a class = \"code-link\" href=\"\" target=\"_blank\">HERE</a>.</p>")
     });
 
 /*
